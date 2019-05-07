2b4328e6c30e274768ac9d45f9abedb3da777298
yuandominic <44766407+yuandominic@users.noreply.github.com>
2019-05-08 01:00:31 +0800

    Calculator

---
+++ Calculator/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+float calculate(const float num1,const char operation,const float num2)
+{
+	switch(operation)
+  {
+
+	case'+': return num1+num2;
+	case'-': return num1-num2;
+	case'*': return num1*num2;
+	case'/': return num1/num2;
+	default: cout<<"Logical Error";
+  }
+	
+	
+	
+}
+
+int main()
+{
+    float a,b;
+    char op;
+   
+   cout<<"Enter a two numbers: ";
+   cin>>a>>b;
+   cout<<"\n Enter Operation : ";
+   cin>>op;
+   
+  cout<<a<<op<<b<<"="<<calculate(a,op,b);
+	
+	
+	
+	
+}
